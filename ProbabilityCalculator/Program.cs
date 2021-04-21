610e71b88364ed048acae33cb2235c7e0b784cae
dave-42r <david@42r.co.uk>
2021-04-21 13:59:07 +0100

    Adding the ability for the user to enter valid decimals to satisfy the requirement. More work needed to make htis production ready.

--- ProbabilityCalculator/Program.cs
+++ ProbabilityCalculator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ProbabilityCalculator.UnitTests;
 
 namespace ProbabilityCalculator
 {
@@ -6,7 +7,15 @@ namespace ProbabilityCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello and welcome to the Probability Calculator");
+            Console.WriteLine("Please enter the first valid probability(0.0 >= a <= 1.0");
+
+            var probabilityA = Decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the second valid probability(0.0 >= b <= 1.0");
+
+            var probabilityB = Decimal.Parse(Console.ReadLine());
+
         }
     }
 }
