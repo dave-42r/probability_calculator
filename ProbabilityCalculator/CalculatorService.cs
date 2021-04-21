7f559b18bb6664f95d7bd54c9dcba917d54434e8
dave-42r <david@42r.co.uk>
2021-04-21 13:45:05 +0100

    Refactor complete, user input validation updated to make the code easier to read and understand what's going on

--- ProbabilityCalculator/CalculatorService.cs
+++ ProbabilityCalculator/CalculatorService.cs
@@ -6,6 +6,11 @@ namespace ProbabilityCalculator.UnitTests
     {
         public void CalculateProbabilities(in decimal probabilityA, in decimal probabilityB)
         {
+            ValidateUserInputs(probabilityA, probabilityB);
+        }
+
+        private static void ValidateUserInputs(decimal probabilityA, decimal probabilityB)
+        {
             if (probabilityA > 1m || probabilityA < 0m)
                 throw new ArgumentException();
             if (probabilityB > 1m || probabilityB < 0m)
